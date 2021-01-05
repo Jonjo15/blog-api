624b62861d2b6339a8e94c11c5bfbea7b14f87b5
IVAN <ivanbatur5g@gmail.com>
2021-01-05 10:49:09 +0100

    Added routes for getting posts, publishing posts

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const User = require("../models/user")
+const Post = require("../models/post")
 const bcrypt = require("bcryptjs")
 const passport = require("passport")
 const utils = require("../utils")
@@ -12,7 +13,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/protected", passport.authenticate('jwt', { session: false }), (req, res, next) => {
-  res.status(200).json({ success: true, msg: "You are successfully authenticated to this route!"});
+  res.status(200).json({ success: true, msg: req.user});
 });
 
 router.post("/register", [
@@ -79,6 +80,60 @@ router.post("/login", (req, res, next) => {
   .catch(err => {
     next(err)
   })
+})
+
+router.post("/posts", passport.authenticate('jwt', { session: false }), [
+
+
+  body('title', 'Title required').trim().isLength({ min: 1 }).escape(),
+  body('body', 'Post Body required').trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+
+    const errors = validationResult(req);
+
+    
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.status(401).json({success: false, msg: "input error"})
+      return;
+    }
+      const post = new Post({
+        title: req.body.title,
+        body: req.body.body,
+        published: false,
+        created_at: Date.now(),
+        comment_count: 0,
+        author: req.user._id
+      })
+      try {
+        post.save()
+            .then((post) => {
+              return res.json({success: true, post, msg: "post created"})
+            });
+
+      } catch (err) {
+        
+        res.json({ success: false, msg: err });
+    
+      }
+}])
+
+router.get("/posts", passport.authenticate('jwt', { session: false }), (req, res, next) => {
+  Post.find({}, 'title body published')
+      .populate('author')
+      .exec(function (err, list_posts) {
+        if (err) { return next(err); }
+        //Successful, so render
+        res.status(200).json({posts: list_posts});
+      });
+})
+
+router.put("/posts/:postId", passport.authenticate('jwt', { session: false }), (req, res, next) => {
+  Post.findByIdAndUpdate(req.params.postId, { published: true}, function updateUser(err) {
+    if (err) {return next(err)}
+    res.json({success: true, msg: "Blog post published"})
+    })
 })
 module.exports = router;
 
